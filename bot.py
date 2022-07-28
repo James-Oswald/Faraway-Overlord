abcedef54a3dfdbcbb4916d9850af353c324ec4f
James Oswald <jamesoswald111@gmail.com>
2022-07-28 10:29:13 +0000

    fixed API

--- bot.py
+++ bot.py
@@ -5,24 +5,38 @@ import random
 from dotenv import load_dotenv
 from TwitterAPI import TwitterAPI,OAuthType
 
-load_dotenv()
+load_dotenv(override=True)
 consumerKey = os.environ["consumer_key"]
 consumerSecret = os.environ["consumer_secret"]
 accessKey = os.environ["access_token_key"]
 accessSecret = os.environ["access_token_secret"]
 
-api = TwitterAPI(
+print(consumerKey)
+print(consumerSecret)
+print(accessKey)
+print(accessSecret)
+
+
+apiV1 = TwitterAPI(
     consumer_key=consumerKey,
     consumer_secret=consumerSecret,
     access_token_key=accessKey,
-    access_token_secret=accessSecret)
+    access_token_secret=accessSecret,
+    api_version="1.1")
+
+apiV2 = TwitterAPI(
+    consumer_key=consumerKey,
+    consumer_secret=consumerSecret,
+    access_token_key=accessKey,
+    access_token_secret=accessSecret,
+    api_version="2")
 
 
 images = os.listdir("images")
 imageName = random.choice(images)
 imageFile = open("./images/"+imageName, 'rb')
 imageData = imageFile.read()
-response = api.request('media/upload', None, {'media': imageData})
+response = apiV1.request('media/upload', None, {'media': imageData})
 
 if response.status_code == 200:
     print("image uploaded sucessfully")
@@ -30,7 +44,7 @@ else:
     print("Failed to upload image:\n" + str(response.json()))
     exit(1)
 
-
+imageName, _ = imageName.split(".") #get rid of file extension
 nameParts = imageName.split("-")
 if len(nameParts) == 5:
     name, season, episode, miniute, sec = nameParts
@@ -45,12 +59,13 @@ name = re.sub(r"(\w)([A-Z])", r"\1 \2", name)
 tweetStr = f"{name} from season {season} ep {episode} at {miniute}:{sec}"
 
 
-response = api.request('statuses/update', {'status': tweetStr, 'media_ids': mediaId})
+response = apiV2.request('tweets', {'text': tweetStr, "media": {"media_ids": [str(mediaId)] }}, method_override="POST")
 
-if response.status_code == 200:
+if response.status_code == 201: 
     print("posted tweet")
 else:
-    print("Failed to post tweet")
+    print(response.status_code)
+    print("Failed to post tweet" + str(response.json()))
     exit(1)
 
 
